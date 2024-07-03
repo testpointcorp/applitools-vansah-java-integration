996859b585b3daafb87585c8d9ddf56205158e54
Shubham Mourya <shubham.mourya@testpoint.com.au>
2024-07-03 19:52:54 +1000

    Update Tests.java

--- src/test/java/com/vansah/example/Tests.java
+++ src/test/java/com/vansah/example/Tests.java
@@ -42,13 +42,8 @@ public class Tests {
 	private int result = 2;
 	private VansahNode vansahTest;
 
-	// Test constants
-	private final static boolean USE_ULTRAFAST_GRID = false;
-	private final static boolean USE_EXECUTION_CLOUD = false;
-
 	// Test control inputs to read once and share for all tests
 	private static String applitoolsApiKey;
-	private static boolean headless;
 
 	// Applitools objects to share for all tests
 	private static BatchInfo batch;
@@ -60,73 +55,44 @@ public class Tests {
 	private Eyes eyes;
 
 	@BeforeMethod()
-	public void setup() throws MalformedURLException {
-		
+	public void setupBeforeEachTest() throws MalformedURLException {
+
 		// Set up Vansah Test Variables
 		vansahTest = new VansahNode();
 		vansahTest.setVansahToken(System.getenv("VANSAH_TOKEN"));
 		vansahTest.setJIRA_ISSUE_KEY(jiraIssueKey);
 		vansahTest.setSPRINT_NAME(testSprint);
-		
+
 		// Read the Applitools API key from an environment variable.
 		applitoolsApiKey = System.getenv("APPLITOOLS_API_KEY");
 
-		// Use headless mode for Continuous Integration (CI) execution.
-		// Use headed mode for local development.
-		headless = false;
-
-		if (USE_ULTRAFAST_GRID) {
-			// Create the runner for the Ultrafast Grid.
-			// Concurrency refers to the number of visual checkpoints Applitools will perform in parallel.
-			// Warning: If you have a free account, then concurrency will be limited to 1.
-			runner = new VisualGridRunner(new RunnerOptions().testConcurrency(1));
-		}
-		else {
-			// Create the Classic runner for local execution.
-			runner = new ClassicRunner();
-		}
+		// Create the Classic runner for local execution.
+		runner = new ClassicRunner();
 
 		// Create a new batch for tests.
-		// A batch is the collection of visual checkpoints for a test suite.
-		// Batches are displayed in the Eyes Test Manager, so use meaningful names.
-		String runnerName = (USE_ULTRAFAST_GRID) ? "Ultrafast Grid" : "Classic runner";
+		String runnerName = "Classic runner";
 		batch = new BatchInfo("UAT Regression Testin on : " + runnerName);
 
 		// Create a configuration for Applitools Eyes.
 		config = new Configuration();
 
 		// Set the Applitools API key so test results are uploaded to your account.
-		// If you don't explicitly set the API key with this call,
-		// then the SDK will automatically read the `APPLITOOLS_API_KEY` environment variable to fetch it.
 		config.setApiKey(applitoolsApiKey);
 
 		// Set the batch for the config.
 		config.setBatch(batch);
 
-		// If running tests on the Ultrafast Grid, configure browsers.
-		if (USE_ULTRAFAST_GRID) {
-			// Add 3 desktop browsers with different viewports for cross-browser testing in the Ultrafast Grid.
-			config.addBrowser(1024, 768, BrowserType.CHROME);
-		}
 		//WebDriver Manager
 		WebDriverManager.chromedriver().setup();
-
-
-		// This method sets up each test with its own ChromeDriver and Applitools Eyes objects.
 
 
 		// Create ChromeDriver options
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--remote-allow-origins=*");
 
-		if (USE_EXECUTION_CLOUD) {
-			// Open the browser remotely in the Execution Cloud.
-			driver = new RemoteWebDriver(new URL(Eyes.getExecutionCloudURL()), options);
-		}
-		else {
-			// Open the browser with a local ChromeDriver instance.
-			driver = new ChromeDriver(options);
-		}
+		// Open the browser with a local ChromeDriver instance.
+		driver = new ChromeDriver(options);
+
 
 		// Set an implicit wait of 10 seconds.
 		// For larger projects, use explicit waits for better control.
@@ -141,37 +107,18 @@ public class Tests {
 		eyes = new Eyes(runner);
 		eyes.setConfiguration(config);
 
-		// Open Eyes to start visual testing.
-		// It is a recommended practice to set all four inputs:
-		
 	}
-	
+
 	@Test()
 	public void login() {
+
 		//Vansah Test Case Key
 		testCaseKey = "TEST-C9";
-		eyes.open(
 
-				// WebDriver object to "watch".
-				driver,
+		//Start a Test
+		eyes.open(driver,"Test App",testCaseKey,new RectangleSize(1200, 600));
 
-				// The name of the application under test.
-				// All tests for the same app should share the same app name.
-				// Set this name wisely: Applitools features rely on a shared app name across tests.
-				"Test App",
-
-				// The name of the test case for the given application.
-				// Additional unique characteristics of the test may also be specified as part of the test name,
-				// such as localization information ("Home Page - EN") or different user permissions ("Login by admin"). 
-				testCaseKey,
-
-				// The viewport size for the local browser.
-				// Eyes will resize the web browser to match the requested viewport size.
-				// This parameter is optional but encouraged in order to produce consistent results.
-				new RectangleSize(1200, 600));
-		
-		// This test covers login for the Applitools demo site, which is a dummy banking app.
-		// Load the login page.
+		// Load the Selenium Vansah Test page.
 		driver.get("https://selenium.vansah.io/");
 
 		// Verify the full login page loaded correctly.
@@ -185,28 +132,13 @@ public class Tests {
 	}
 	@Test()
 	public void landingPage() {
+
 		//Vansah Test Case Key
 		testCaseKey = "TEST-C8";
-		eyes.open(
 
-				// WebDriver object to "watch".
-				driver,
+		//Start a Test
+		eyes.open(driver,"Test App",testCaseKey,new RectangleSize(1200, 600));
 
-				// The name of the application under test.
-				// All tests for the same app should share the same app name.
-				// Set this name wisely: Applitools features rely on a shared app name across tests.
-				"Test App",
-
-				// The name of the test case for the given application.
-				// Additional unique characteristics of the test may also be specified as part of the test name,
-				// such as localization information ("Home Page - EN") or different user permissions ("Login by admin"). 
-				testCaseKey,
-
-				// The viewport size for the local browser.
-				// Eyes will resize the web browser to match the requested viewport size.
-				// This parameter is optional but encouraged in order to produce consistent results.
-				new RectangleSize(1200, 600));
-		
 		// Load the login page.
 		driver.get("https://selenium.vansah.io/");
 
@@ -215,31 +147,22 @@ public class Tests {
 	}
 
 	@AfterMethod
-	public void cleanUpTest() throws Exception {
+	public void cleanUpAfterEachTest() throws Exception {
 
 		// Close Eyes to tell the server it should display the results.
 		eyes.closeAsync();
 
 		// Quit the WebDriver instance.
 		driver.quit();
-		// Close the batch and report visual differences to the console.
-		// Note that it forces JUnit to wait synchronously for all visual checkpoints to complete.
 		try {
-		TestResultsSummary allTestResults = runner.getAllTestResults();
-		System.out.println(allTestResults);}
+			TestResultsSummary allTestResults = runner.getAllTestResults();
+			System.out.println(allTestResults);}
 		catch(DiffsFoundException e) {
 			result = 1;
 			System.out.println(e.getMessage());
 		}finally {
 			vansahTest.addQuickTestFromJiraIssue(testCaseKey, result);
 		}
-
-		// Warning: `eyes.closeAsync()` will NOT wait for visual checkpoints to complete.
-		// You will need to check the Eyes Test Manager for visual results per checkpoint.
-		// Note that "unresolved" and "failed" visual checkpoints will not cause the JUnit test to fail.
-
-		// If you want the JUnit test to wait synchronously for all checkpoints to complete, then use `eyes.close()`.
-		// If any checkpoints are unresolved or failed, then `eyes.close()` will make the JUnit test fail.
 	}
 
 }
